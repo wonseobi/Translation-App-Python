efd6aa80f66b39841c814d5b346c58fa1625909e
Won Lee <wonseob2207@gmail.com>
2024-12-09 17:29:12 -0600

    added user input fields

--- translation_app.py
+++ translation_app.py
@@ -22,14 +22,21 @@ label_server.pack()
 entry_server = tk.Entry(root)
 entry_server.pack()
 
-# server address
-label_server = tk.Label(root, text=" Server Address")
-label_server.pack()
-entry_server = tk.Entry(root)
-entry_server.pack()
+# username
+label_username = tk.Label(root, text=" Username")
+label_username.pack()
+entry_username = tk.Entry(root)
+entry_username.pack()
 
-# server address
-label_server = tk.Label(root, text=" Server Address")
-label_server.pack()
-entry_server = tk.Entry(root)
-entry_server.pack()
+# password
+label_password = tk.Label(root, text=" Password ")
+label_password.pack()
+entry_password = tk.Entry(root)
+entry_password.pack()
+
+# connecting to the button
+connect_button = tk.Button(root, text="Connect", command=connect_to_vpn)
+connect_button.pack()
+
+# run the main loop
+root.mainloop()
